f254dce0ecd158decc92a4e89286097de21c05a9
Swapnil8788 <136966398+Swapnil8788@users.noreply.github.com>
2023-07-11 01:41:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
+// edit this file
 const dbPath = path.join(__dirname, "goodreads.db");
 const app = express();
 
